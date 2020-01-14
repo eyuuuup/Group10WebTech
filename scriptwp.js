25f706d19efa0535171c0d1f144ea1b83351724b
Eyüp Vroegop <eyupvroegop@gmail.com>
2020-01-14 15:30:31 +0100

    Cleaned up the javascript

--- scriptwp.js
+++ scriptwp.js
@@ -1,3 +1,38 @@
+function purgeTheHeretics(data)  {
+    console.log(JSON.stringify(data));
+
+    //https://stackoverflow.com/questions/17724017/using-jquery-to-build-table-rows-from-ajax-responsejson
+    $.each(data, function(i, item) {
+        var $tr = $("<tr>").append(
+         
+            $('<th>').text(item.brand),
+            $('<td>').text(item.model),
+            $('<td>').text(item.os),
+            $('<td>').html("<img src=" + item.image + ">"),
+            $('<td>').text(item.screensize),
+            
+        );
+        $("#test").append($tr); 
+         
+    });
+
+    // https://stackoverflow.com/questions/14891216/how-to-use-datatable-with-dynamically-created-table
+    // https://stackoverflow.com/questions/13708781/datatables-warningtable-id-example-cannot-reinitialise-data-table
+    $('#actual').DataTable(
+        {
+            paging: false,
+            searching: false,
+            bInfo: false,
+            retrieve: true,
+            columnDefs: [
+                {
+                    orderable : false, targets: 3
+                }
+            ]
+        }       
+    );
+}
+
 $(document).ready(
     function() {
         $.ajax(
@@ -5,170 +40,78 @@ $(document).ready(
             url: "https://wt.ops.labs.vu.nl/api20/5824a3f6",
             method: "GET",
             dataType: "json"
-        })
-        .done(
-            
+            }
+        ).done(
             function(data) {
-                console.log(JSON.stringify(data));
-                //https://stackoverflow.com/questions/17724017/using-jquery-to-build-table-rows-from-ajax-responsejson
-                $.each(data, function(i, item) {
-                    var $tr = $("<tr>").append(
-                     
-                        $('<th>').text(item.brand),
-                        $('<td>').text(item.model),
-                        $('<td>').text(item.os),
-                        $('<td>').html("<img src=" + item.image + ">"),
-                        $('<td>').text(item.screensize),
-                        
-                    );
-                    $("#test").append($tr); 
-                    
-                    
-                });
-                // https://stackoverflow.com/questions/14891216/how-to-use-datatable-with-dynamically-created-table
-                // https://stackoverflow.com/questions/13708781/datatables-warningtable-id-example-cannot-reinitialise-data-table
-                $('#actual').DataTable(
+                purgeTheHeretics(data)
+            }  
+        );
+    }
+);
+
+$(document).ready(
+    function() {
+        $('#reset').click( 
+            function () {
+                $('#actual').DataTable().destroy();
+                $('#test').empty();        
+
+                $.ajax(
                     {
-                        paging: false,
-                        searching: false,
-                        bInfo: false,
-                        retrieve: true,
-                    
-                        columnDefs: [
+                    url: "https://wt.ops.labs.vu.nl/api20/5824a3f6/reset",
+                    method: "GET",
+                    dataType: "json"
+                    }
+                ).done(
+                    function() {
+                        $.ajax(
                             {
-                                orderable : false, targets: 3
+                                url: "https://wt.ops.labs.vu.nl/api20/5824a3f6",
+                                method: "GET",
+                                dataType: "json"
                             }
-                        ]
-                    }       
+                        ).done(
+                            function(data) {
+                                purgeTheHeretics(data);
+                                alert("Database has been reset");
+                            }
+                        );
+                    }
                 );
             }
         );
     }
 );
 
-
-
-
 $(document).ready(
     function() {
-        $('#reset').click( function () {
-            $.ajax(
-                {
-                url: "https://wt.ops.labs.vu.nl/api20/5824a3f6/reset",
-                method: "GET",
-                dataType: "json"
-            })
-            .done(
-                function() {
-                    alert("Database has been reset");
-                    $('#actual').DataTable().destroy();
-                    $('#test').empty();
+        $('form').submit(
+            function() {
+
+                // https://stackoverflow.com/questions/6000073/how-can-i-remove-everything-inside-of-a-div
+                $('#actual').DataTable().destroy();
+                $('#test').empty();
+                
+                //https://stackoverflow.com/questions/1960240/jquery-ajax-submit-form
+                //https://stackoverflow.com/questions/6230964/waiting-for-post-to-finish
+                $.post('https://wt.ops.labs.vu.nl/api20/5824a3f6', $('form').serialize(), function(){
+
+                    //https://stackoverflow.com/questions/4038567/prevent-redirect-after-form-is-submitted
                     $.ajax(
                         {
                         url: "https://wt.ops.labs.vu.nl/api20/5824a3f6",
                         method: "GET",
                         dataType: "json"
-                    }).done(
-           
+                        }
+                    ).done(
                         function(data) {
-                            console.log(JSON.stringify(data));
-                          
-                            $.each(data, function(i, item) {
-                                var $tr = $("<tr>").append(
-                                 
-                                    $('<th>').text(item.brand),
-                                    $('<td>').text(item.model),
-                                    $('<td>').text(item.os),
-                                    $('<td>').html("<img src=" + item.image + ">"),
-                                    $('<td>').text(item.screensize),
-                                    
-                                );
-                                $("#test").append($tr); 
-                      
-                                
-                                
-                            });
-                            $('#actual').DataTable(
-                                {
-                                    paging: false,
-                                    searching: false,
-                                    bInfo: false,
-                                    retrieve: true,
-                                
-                                    columnDefs: [
-                                        {
-                                            orderable : false, targets: 3
-                                        }
-                                    ]
-                                }       
-                            );
-                           
-                            }
-                           
-
-                        )
-                
-                }
-            );
-            
-        });
+                            purgeTheHeretics(data);
+                            alert("Product has been added");
+                        }
+                    );
+                });
+                return false;
+            }
+        );
     }
 );
-
-$(function() {
-    
-    $('form').submit(
-    function() {
-        
-        // https://stackoverflow.com/questions/6000073/how-can-i-remove-everything-inside-of-a-div
-        $('#actual').DataTable().destroy();
-        $('#test').empty();
-        //https://stackoverflow.com/questions/1960240/jquery-ajax-submit-form
-        $.post('https://wt.ops.labs.vu.nl/api20/5824a3f6', $('form').serialize())
-        //https://stackoverflow.com/questions/4038567/prevent-redirect-after-form-is-submitted
-        $.ajax(
-            {
-            url: "https://wt.ops.labs.vu.nl/api20/5824a3f6",
-            method: "GET",
-            dataType: "json"
-            })
-        .done(
-           
-            function(data) {
-                console.log(JSON.stringify(data));
-              
-                $.each(data, function(i, item) {
-                    var $tr = $("<tr>").append(
-                     
-                        $('<th>').text(item.brand),
-                        $('<td>').text(item.model),
-                        $('<td>').text(item.os),
-                        $('<td>').html("<img src=" + item.image + ">"),
-                        $('<td>').text(item.screensize),
-                    );
-                    $("#test").append($tr); 
-          
-                    
-                    
-                });
-                $('#actual').DataTable(
-                    {
-                        paging: false,
-                        searching: false,
-                        bInfo: false,
-                        retrieve: true,
-                    
-                        columnDefs: [
-                            {
-                                orderable : false, targets: 3
-                            }
-                        ]
-                    }       
-                );
-                }
-            )
-            return false;
-        }
-    );
-});
-
