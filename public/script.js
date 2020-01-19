df77755ab19b3de43ba8daa3ba5b14428609ecb9
eyupams <43216769+eyupams@users.noreply.github.com>
2020-01-19 20:16:45 +0100

    Added PUT to assignment 3

--- public/script.js
+++ public/script.js
@@ -3,8 +3,8 @@ function initializeData(data)  {
     //https://stackoverflow.com/questions/17724017/using-jquery-to-build-table-rows-from-ajax-responsejson
     $.each(data, function(i, item) {
         var $tr = $("<tr>").append(
-         
-            $('<th>').text(item.brand),
+            $('<th>').text(item.id),
+            $('<td>').text(item.brand),
             $('<td>').text(item.model),
             $('<td>').text(item.os),
             $('<td>').html("<img src=" + item.image + ">"),
@@ -81,6 +81,55 @@ $(document).ready(
         );
     }
 );
+
+$(document).ready(
+    function() {
+        $('#update').click(
+            function() {
+
+                // https://stackoverflow.com/questions/6000073/how-can-i-remove-everything-inside-of-a-div
+                $('#actual').DataTable().destroy();
+                $('#test').empty();
+
+                 //https://stackoverflow.com/questions/11338774/serialize-form-data-to-json
+                 var unindexed_array = $('form').serializeArray();
+                 var indexed_array = {};
+ 
+                 $.map(unindexed_array, function(n, i){
+                     indexed_array[n['name']] = n['value'];
+                 });
+                
+                //https://stackoverflow.com/questions/1960240/jquery-ajax-submit-form
+                //https://stackoverflow.com/questions/6230964/waiting-for-post-to-finish
+                $.ajax(
+                    {
+                        url:'http://localhost:3000/api/products/' + indexed_array.ID,
+                        method: "PUT",
+                        dataType: "json",
+                        contentType: "application/json",
+                        data: JSON.stringify(indexed_array)
+                    }
+                 ).done (function (){
+                     //https://stackoverflow.com/questions/4038567/prevent-redirect-after-form-is-submitted
+                     $.ajax(
+                        {
+                        url: "http://localhost:3000/api/products",
+                        method: "GET",
+                        dataType: "json"
+                        }
+                    ).done(
+                        function(data) {
+                            initializeData(data);
+                            alert("Product has been added");
+                        }
+                    );
+                });
+                return false;
+            }
+        );
+    }
+);
+
 
 $(document).ready(
     function() {
