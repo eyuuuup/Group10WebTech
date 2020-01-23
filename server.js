703f7277f696e7e0276bb42a94cb6b0a330c6314
eyupams <43216769+eyupams@users.noreply.github.com>
2020-01-23 18:16:08 +0100

    Cleaned up server.js

--- server.js
+++ server.js
@@ -26,9 +26,13 @@ app.use(express.static('public'))
 // /api/products GET
 // https://stackoverflow.com/questions/19041837/difference-between-res-send-and-res-json-in-express-js
 router.get("/products", function(req, res) {
-    db.all("SELECT * FROM products", function(err, row){
+    db.all("SELECT * FROM products", function(err, row) {
         if (err) {
-            res.status(400).json({"error": err.message});
+            res.status(400).json(
+                {
+                    "error": err.message
+                }
+            );
         } else {
             res.status(200).json(row);
         }
@@ -42,33 +46,42 @@ router.post("/products", function(req, res) {
     
     db.run("INSERT INTO products (brand, model, os, image, screensize) VALUES (?,?,?,?,?)", params, function(err, result) {
         if (err) {
-            res.status(400).json({"error": err.message});
+            res.status(400).json(
+                {
+                    "error": err.message
+                }
+            );
             return;
         } else {
-            res.status(201).json({
-                "message": "success",
-                "data": req.body,
-                "id": this.lastID
-            })
+            res.status(201).json(
+                {
+                    "message": "success",
+                    "data": req.body,
+                    "id": this.lastID
+                }
+            );
         }
-    })
-    
+    });
 });
 
 // /api/products/id GET
 router.get("/products/:id", function(req, res) {
     var params = [req.params.id]
-    db.get("SELECT * FROM products where id = ?", params, function(err, row){
+
+    db.get("SELECT * FROM products where id = ?", params, function(err, row) {
         if (err) {
-            res.status(400).json({"error":err.message})
+            res.status(400).json(
+                {
+                    "error":err.message
+                }
+            );
             return;
-        } else if (row === undefined){
+        } else if (row === undefined) {
             res.sendStatus(404);
         } else {
             res.json(row);
         }
     });
-
 });
 
 // /api/products/id UPDATE
@@ -76,14 +89,18 @@ router.get("/products/:id", function(req, res) {
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
 router.put("/products/:id", function(req, res) {
     var params = [req.body.brand, req.body.model, req.body.os, req.body.image, req.body.screensize, req.params.id];
+
     db.run("UPDATE products SET brand = ?, model = ?, os = ?, image = ?, screensize = ? WHERE id = ?", 
-    params, function(err, result){
+    params, function(err, result) {
         if (err) {
-            res.status(400).json({"error":err.message})
+            res.status(400).json(
+                {
+                    "error": err.message
+                }
+            );
             return;
         } else if (this.changes == 0) {
             res.sendStatus(404);
-
         } else {
             res.sendStatus(204);
         }
@@ -93,55 +110,70 @@ router.put("/products/:id", function(req, res) {
 
 // /api/products/reset DELETE
 router.delete("/products/reset", function(req, res) {
-    db.run("DELETE FROM products", function(err, result){
+    db.run("DELETE FROM products", function(err, result) {
         if(err) {
-            res.status(400).json({"error": res.message});
+            res.status(400).json(
+                {
+                    "error": err.message
+                }
+            );
             return;
         } else {
             res.sendStatus(204);
         }
-    })
-})
+    });
+});
 
 // /api/products/id DELETE
 router.delete("/products/:id", function(req, res) {
     var params = [req.params.id];
+
     db.run("DELETE FROM products WHERE id = ?", params, 
-    function(err, result){
+    function(err, result) {
         if(err) {
-            res.status(400).json({"error": res.message});
+            res.status(400).json(
+                {
+                    "error": err.message
+                }
+            );
             return;
         } else if (this.changes == 0) {
             res.sendStatus(404);    
         } else {
             res.sendStatus(204);
         }
-    })
-})
-
-// check if server is online
-router.get("/", function(req, res, next) {
-    res.status(200).json({"message":"server online"});
+    });
 });
 
+// check if api is online
+router.get("/", function(req, res) {
+    res.status(200).json(
+        {
+            "message":"api online"
+        }
+    );
+});
 
-app.use(function(req, res){
+// everything else throws a 404
+app.use(function(req, res, next) {
     res.sendStatus(404);
 });
 
-// https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
-// https://stackoverflow.com/questions/21864127/nodejs-process-hangs-on-exit-ctrlc
-process.on("exit", function(){
+/*  https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+    https://stackoverflow.com/questions/21864127/nodejs-process-hangs-on-exit-ctrlc
+    close the database first before closing the application */
+process.on("exit", function() {
     console.log("Closing the database connection.");
-    db.close(function(err){
+    db.close(function(err) {
         if (err) {
             return console.error(err.message);
         } else {
             console.log("Closed the database connection.");
         }
+    });
 });
-})
 
-process.on("SIGINT", function(){
+// call procces exit on ctrl-c
+process.on("SIGINT", function() {
     process.exit();
 });
